8fac9d225305338f714cdf175b289c94e6abbf7b
luanrrsouza <luanrrdsouza@gmail.com>
2025-05-07 00:29:55 -0300

    feat: Adicionando o Sobre à página.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { About } from "./_components/About";
 import { Hero } from "./_components/Hero";
+
 
 function Home() {
   return (
     <main>
       <Hero />
+      <About/>
     </main>
   );
 }
